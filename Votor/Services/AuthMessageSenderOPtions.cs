3d647bbf1da3dc7efcc9467681036c509cedf0ac
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2018-08-29 20:39:05 +0200

    Generated project files + EmailSender
    Generated asp.net core mvc project
    SendGrid

---
+++ Votor/Services/AuthMessageSenderOPtions.cs
@@ -0,0 +1,8 @@
+﻿namespace Votor.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
