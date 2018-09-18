903bd3c23a1fd2677986dbd29b7813b53958caba
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2018-09-18 20:23:02 +0200

    Localization of validation error messages
    + adaption of identity policies

--- Votor/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Votor/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -39,17 +39,20 @@ namespace Votor.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "The {0} field is required.")]
             [DataType(DataType.Text)]
-            [Display(Name = "UserName")]
+            [Display(Name = "Username")]
             public string UserName { get; set; }
 
-            [Required]
-            [EmailAddress]
+            [Required(ErrorMessage = "The {0} field is required.")]
+            [EmailAddress(ErrorMessage = "The {0} field is not a valid e-mail address.")]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            [Required]
+            //The Email field is required.
+            //, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long."
+
+            [Required(ErrorMessage = "The {0} field is required.")]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
