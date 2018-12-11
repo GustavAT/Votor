16ed2dab1580203324b10e8b4192da5091913d85
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2018-12-11 23:08:31 +0100

    Added CORS to api methods

--- Votor/Controllers/VotorController.cs
+++ Votor/Controllers/VotorController.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Votor.Areas.Portal.Data;
-using Votor.Areas.Portal.Models;
 
 namespace Votor.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("WedenigsPolicy")]
     public class VotorController : ControllerBase
     {
         private readonly VotorContext _context;
